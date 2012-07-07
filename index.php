b0193bfea56d4c240532ce8c142b09ddb7737801
Mauricio <mauricioaizaga@gmail.com>
2012-07-07 15:11:12 -0500

    maquetando y poniendo estilos

--- index.php
+++ index.php
@@ -3,16 +3,20 @@
     <head>
         <meta charset="utf-8" >
         <title>Prueba de Ramas</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
         <header>
             <h1>Git Branchs</h1>
         </header>
-        <section>
-            <h2>Ejemplo de Ramas.</h2>
-            <p>Este es un ejemplo de ramas</p>
-            <code>Este codigo fue escrito en el master</code>
-        </section>
+        <div id="container" class="b">
+            <section class="b">
+                <h2>Ejemplo de Ramas.</h2>
+                <p>Este es un ejemplo de ramas</p>
+                <code>Este codigo fue escrito en el master</code>
+            </section>
+        </div>
         <footer>
             <p>@MaoAiz</p>
         </footer>
