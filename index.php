9e846c7cdacd11628df4f0026ce6b67a5b26c7f1
Mauricio <mauricioaizaga@gmail.com>
2012-07-07 14:57:40 -0500

    Agregando contenido al index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" >
+        <title>Prueba de Ramas</title>
+    </head>
+    <body>
+        <header>
+            <h1>Git Branchs</h1>
+        </header>
+        <section>
+            <h2>Ejemplo de Ramas.</h2>
+            <p>Este es un ejemplo de ramas</p>
+            <code>Este codigo fue escrito en el master</code>
+        </section>
+        <footer>
+            <p>@MaoAiz</p>
+        </footer>
+    </body>
+</html>
